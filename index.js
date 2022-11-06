7da9e97817ef5413d9f820823bf046dfb6c64be9
Tristyn Broadhead <trisclassie@gmail.com>
2022-11-06 12:13:22 -0700

    commit tree da78293ac8db493bcea479aeb6612c2b80e4bc48

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+// const superbowlWin = record.find(records => records.result === "W")
+
+// console.log(superbowlWin.year)
+
+function superbowlWin(records){
+    let obj = records.find(records => records.result === "W")
+    if(obj){
+        return obj.year
+    }
+    return obj
+}
+
+
